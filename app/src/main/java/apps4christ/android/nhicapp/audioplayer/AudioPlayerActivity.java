ed624c7e8f2e92490d834548839b31f7b316c285
Mark Merin <mjmerin@hotmail.com>
2015-03-10 18:53:21 -0700

    Fix for Issue #1
    Add AudioPlayerActivity class

---
+++ app/src/main/java/apps4christ/android/nhicapp/audioplayer/AudioPlayerActivity.java
@@ -0,0 +1,248 @@
+package apps4christ.android.nhicapp.audioplayer;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import apps4christ.android.nhicapp.R;
+
+public class AudioPlayerActivity extends Activity implements SeekBar.OnSeekBarChangeListener {
+
+	private ImageButton btnPlay;
+	private ImageButton btnForward;
+	private ImageButton btnBackward;
+	private ImageButton btnNext;
+	private ImageButton btnPrevious;
+	private SeekBar songProgressBar;
+	private TextView songTitleLabel;
+	private TextView songCurrentDurationLabel;
+	private TextView songTotalDurationLabel;
+	// Media Player
+	private  MediaPlayer mp;
+	// Handler to update UI timer, progress bar etc,.
+	private Handler mHandler = new Handler();;
+	private Utilities utils;
+	private int seekForwardTime = 5000; // 5000 milliseconds
+	private int seekBackwardTime = 5000; // 5000 milliseconds
+
+    private Intent intent;
+    private String url;
+    private String podcastTitle;
+	
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.player);
+		
+		// All player buttons
+		btnPlay = (ImageButton) findViewById(R.id.btnPlay);
+		btnForward = (ImageButton) findViewById(R.id.btnForward);
+		btnBackward = (ImageButton) findViewById(R.id.btnBackward);
+		songProgressBar = (SeekBar) findViewById(R.id.songProgressBar);
+		songTitleLabel = (TextView) findViewById(R.id.songTitle);
+		songCurrentDurationLabel = (TextView) findViewById(R.id.songCurrentDurationLabel);
+		songTotalDurationLabel = (TextView) findViewById(R.id.songTotalDurationLabel);
+		
+		// Mediaplayer
+		mp = new MediaPlayer();
+		utils = new Utilities();
+
+        intent = getIntent();
+        url = intent.getStringExtra("url");
+        podcastTitle = intent.getStringExtra("title");
+		
+		// Listeners
+		songProgressBar.setOnSeekBarChangeListener(this); // Important
+		//mp.setOnCompletionListener(this); // Important
+
+		// By default play first song
+		playPodcast(url);
+
+				
+		/**
+		 * Play button click event
+		 * plays a song and changes button to pause image
+		 * pauses a song and changes button to play image
+		 * */
+		btnPlay.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// check for already playing
+				if(mp.isPlaying()){
+					if(mp!=null){
+						mp.pause();
+						// Changing button image to play button
+						btnPlay.setImageResource(R.drawable.btn_play);
+					}
+				}else{
+					// Resume song
+					if(mp!=null){
+						mp.start();
+						// Changing button image to pause button
+						btnPlay.setImageResource(R.drawable.btn_pause);
+					}
+				}
+				
+			}
+		});
+		
+		/**
+		 * Forward button click event
+		 * Forwards song specified seconds
+		 * */
+		btnForward.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// get current song position				
+				int currentPosition = mp.getCurrentPosition();
+				// check if seekForward time is lesser than song duration
+				if(currentPosition + seekForwardTime <= mp.getDuration()){
+					// forward song
+					mp.seekTo(currentPosition + seekForwardTime);
+				}else{
+					// forward to end position
+					mp.seekTo(mp.getDuration());
+				}
+			}
+		});
+		
+		/**
+		 * Backward button click event
+		 * Backward song to specified seconds
+		 * */
+		btnBackward.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// get current song position				
+				int currentPosition = mp.getCurrentPosition();
+				// check if seekBackward time is greater than 0 sec
+				if(currentPosition - seekBackwardTime >= 0){
+					// forward song
+					mp.seekTo(currentPosition - seekBackwardTime);
+				}else{
+					// backward to starting position
+					mp.seekTo(0);
+				}
+				
+			}
+		});
+
+	}
+
+	/**
+	 * Function to play a song
+	 * @param url - url of podcast
+	 * */
+	public void  playPodcast(String url){
+		// Play podcast
+		try {
+
+        	mp.reset();
+            mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
+			mp.setDataSource(url);
+			mp.prepare();
+			mp.start();
+			// Displaying Song title
+			String songTitle = "Test Sermon";
+        	songTitleLabel.setText(podcastTitle);
+			
+        	// Changing Button Image to pause image
+			btnPlay.setImageResource(R.drawable.btn_pause);
+			
+			// set Progress bar values
+			songProgressBar.setProgress(0);
+			songProgressBar.setMax(100);
+			
+			// Updating progress bar
+			updateProgressBar();			
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Update timer on seekbar
+	 * */
+	public void updateProgressBar() {
+        mHandler.postDelayed(mUpdateTimeTask, 100);        
+    }	
+	
+	/**
+	 * Background Runnable thread
+	 * */
+	private Runnable mUpdateTimeTask = new Runnable() {
+		   public void run() {
+			   long totalDuration = mp.getDuration();
+			   long currentDuration = mp.getCurrentPosition();
+			  
+			   // Displaying Total Duration time
+			   songTotalDurationLabel.setText(""+utils.milliSecondsToTimer(totalDuration));
+			   // Displaying time completed playing
+			   songCurrentDurationLabel.setText(""+utils.milliSecondsToTimer(currentDuration));
+			   
+			   // Updating progress bar
+			   int progress = (int)(utils.getProgressPercentage(currentDuration, totalDuration));
+			   //Log.d("Progress", ""+progress);
+			   songProgressBar.setProgress(progress);
+			   
+			   // Running this thread after 100 milliseconds
+		       mHandler.postDelayed(this, 100);
+		   }
+		};
+		
+	/**
+	 * 
+	 * */
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromTouch) {
+		
+	}
+
+	/**
+	 * When user starts moving the progress handler
+	 * */
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// remove message Handler from updating progress bar
+		mHandler.removeCallbacks(mUpdateTimeTask);
+    }
+	
+	/**
+	 * When user stops moving the progress hanlder
+	 * */
+	@Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+		mHandler.removeCallbacks(mUpdateTimeTask);
+		int totalDuration = mp.getDuration();
+		int currentPosition = utils.progressToTimer(seekBar.getProgress(), totalDuration);
+		
+		// forward or backward to certain seconds
+		mp.seekTo(currentPosition);
+		
+		// update timer progress again
+		updateProgressBar();
+    }
+	
+	@Override
+	 public void onDestroy(){
+	 super.onDestroy();
+        mp.stop();
+	 }
+	
+}
